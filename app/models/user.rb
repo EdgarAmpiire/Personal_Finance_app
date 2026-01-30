57e666e063d2318df64c375fccdc5364477454d6
Edgar Ampiire <eampiire@gmail.com.com>
2026-01-30 18:04:14 +0300

    ran robocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
 
-  private 
+  private
 
   def normalize_email
     self.email = email.to_s.strip.downcase
